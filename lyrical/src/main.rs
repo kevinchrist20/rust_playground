b80ae99f438743632675a3f61ab5adb17ba95db2
kevinchrist <kevinamevor@outlook.com>
2024-05-31 20:08:09 +0000

    Made artist and song_title as command line arguments

--- lyrical/src/main.rs
+++ lyrical/src/main.rs
@@ -1,12 +1,24 @@
-use scraper::Selector;
 use html2text;
+use scraper::Selector;
+
+use std::env::{self};
 use std::{fs, io};
 
 const BASE_URL: &str = "https://www.azlyrics.com";
 
 #[tokio::main]
 async fn main() {
-    match get_lyrics("eminem", "houdini").await {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Argument must consists of artist and song title.");
+        return;
+    }
+
+    let artist = &args[1];
+    let song_title = &args[2];
+
+    match get_lyrics(&artist, &song_title).await {
         Ok(()) => println!("Thank you for coming. Until next time!!"),
         Err(err) => eprintln!("An error occurred: {}", err),
     }
@@ -15,6 +27,7 @@ async fn main() {
 async fn get_lyrics(artist: &str, song: &str) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("{BASE_URL}/lyrics/{artist}/{song}.html");
 
+    println!("BASE_URL: {}", url);
     let response = reqwest::get(url).await?;
     let html_content = response.text().await?;
 
@@ -32,8 +45,7 @@ async fn get_lyrics(artist: &str, song: &str) -> Result<(), Box<dyn std::error::
 
     let filename = format!("{artist}_{song}.txt");
     let mut lyrics_content = String::from("");
-    
-    // Print the filtered divs
+
     for div in lyrics_div {
         let html = div.inner_html();
         let text = html2text::from_read(html.as_bytes(), 80);
@@ -41,6 +53,7 @@ async fn get_lyrics(artist: &str, song: &str) -> Result<(), Box<dyn std::error::
         lyrics_content.push_str(&text);
     }
 
+    println!("{}", lyrics_content);
     write_to_file(lyrics_content, &filename)?;
 
     Ok(())
